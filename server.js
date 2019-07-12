a6526a64632cb6945ad8639e3bcbe3b5cac75ef0
Eric Chavez <eric@emcaudio.com>
2019-07-11 20:22:56 -0500

    added now.json

--- server.js
+++ server.js
@@ -3,40 +3,48 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var router = express.Router();
-let cards = [];
-let catalog = [];
+let cards = [
+	{
+		id: '7619e680-a43d-11e9-aa8a-7569a0b7c047',
+		title: 'Card Title',
+		content: 'This is where I would put the content, if I had any'
+	}
+];
+let swimlanes = [
+	{
+		id: 'abab6e90-a43d-11e9-b378-fdce31a25b77',
+		title: 'Swimlane Title',
+		cards: [ '7619e680-a43d-11e9-aa8a-7569a0b7c047' ]
+	}
+];
 
-app.use(express.static('static'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3001;
 
 app.use('/', router);
 
-router.post('/cards', function(req, res) {
+router.post('/card', function(req, res) {
 	cards.push(req.body);
 	res.send('message resceved');
 });
-router.get('/cards', function(req, res) {
-	res.send(cards);
-});
-router.get('/cards/:card_id', function(req, res) {
+router.get('/card/:card_id', function(req, res) {
 	res.send(cards.find((card) => card.id == req.params.card_id));
 });
-router.put('/cards/:card_id', function(req, res) {
+router.put('/card/:card_id', function(req, res) {
 	let update = cards.find((card) => card.id == req.params.card_id);
 	update.color = req.body.color || update.color;
 	update.body = req.body.body || update.body;
 	update.title = req.body.title || update.title;
 });
 
-router.post('/catalog', function(req, res) {
-	catalog = req.body;
+router.post('/swimlane', function(req, res) {
+	swimlanes.push(req.body);
 	res.send('message resceved');
 });
-router.get('/catalog', function(req, res) {
-	res.send(catalog.catalog);
+router.get('/swimlanes', function(req, res) {
+	res.send(swimlanes);
 });
 app.listen(port);
 console.log('Uplink acquired on port ' + port);
